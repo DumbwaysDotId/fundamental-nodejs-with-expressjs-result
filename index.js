8d9e434cb30f3fc03eda2958c66fa07b346f0c1b
Jody Septiawan <jody@192.168.1.2>
2021-07-05 14:17:01 +0700

    Hello World with ExpressJs

---
+++ index.js
@@ -0,0 +1,17 @@
+//instantiate express module
+const express = require('express')
+
+//use express in app variable
+const app = express()
+
+//define the server port
+const port = 5000
+
+//create the homepage route
+app.get('/', (req, res) => {
+    //res means, response, and it send string "Hello Express!" to the API
+    res.send('Hello Express!')
+})    
+
+//when this nodejs app executed, it will listen to defined port
+app.listen(port, () => console.log(`Listening on port ${port}!`))
