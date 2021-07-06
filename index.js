36c7bc843249bedb3f1cd46d0818ab4f765d4f93
Jody Septiawan <jody@192.168.1.2>
2021-07-06 10:25:24 +0700

    Routing

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-//instantiate express module
 const express = require('express')
 
-//use express in app variable
 const app = express()
-
-//define the server port
 const port = 5000
 
-//create the homepage route
-app.get('/', (req, res) => {
-    //res means, response, and it send string "Hello Express!" to the API
-    res.send('Hello Express!')
-})    
+//allow this app to receive incoming json request
+app.use(express.json())
 
-//when this nodejs app executed, it will listen to defined port
+let todos = [
+    {
+        id: 1,
+        title: "Cuci tangan",
+        isDone: true
+    },
+    {
+        id: 2,
+        title: "Jaga jarak",
+        isDone: false
+    },
+]
+
+//GET list route: simply send arr of obj todos on your user screen
+app.get('/todos', (req, res) => {
+    res.send({ data: todos });
+})
+
+//GET detail route: send the todo obj, by received id request params
+app.get('/todo/:id', (req, res) => {
+    const id = req.params.id
+    const index = id - 1    
+    res.send(todos[index])
+})
+
+//POST route: receive json body request, from user input, then push to todos array
+app.post('/todo', (req, res) => {
+    todos = [...todos, req.body];
+    res.send({ data: todos });
+})
+
+//PATCH route: receive json body request, from user input, then push to todos array
+//by object id
+app.patch('/todo/:id', (req, res) => {
+    const { id } = req.params;
+    todos[id - 1] = { ...todos[id - 1], ...req.body };
+    res.send({ data: todos[id - 1] });
+
+})
+
+//DELETE route: delete the todo obj, by received id request params
+app.delete('/todo/:id', (req, res) => {
+    const { id } = req.params;
+    todos = todos.filter((todo) => todo.id != id);
+    res.send({ data: todos });
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}!`))
